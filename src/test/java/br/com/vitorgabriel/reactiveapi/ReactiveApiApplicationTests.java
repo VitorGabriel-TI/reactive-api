5567d30c2530a415fe0107e09b400bfabe542e78
Vitor Gabriel <contato.vitorgabrielti@gmail.com>
2023-12-19 16:40:34 -0300

    att

--- src/test/java/br/com/vitorgabriel/reactiveapi/ReactiveApiApplicationTests.java
+++ src/test/java/br/com/vitorgabriel/reactiveapi/ReactiveApiApplicationTests.java
@@ -1,13 +1,44 @@
 package br.com.vitorgabriel.reactiveapi;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ReactiveApiApplicationTests {
+	@Autowired
+	private WebTestClient webClient;
 
 	@Test
-	void contextLoads() {
+	void test() {
+		User user = new User(null, "user", "123456", "user@email.com");
+		webClient.post().uri("/users").bodyValue(user)
+		.exchange()
+		.expectStatus().is2xxSuccessful()
+		.expectBody(User.class)
+		.value(postResponse ->{
+			assertNotNull(postResponse.id());
+			assertEquals(user.username(), postResponse.username());
+			assertEquals(user.email(), postResponse.email());
+			assertEquals(user.password(), postResponse.password());
+		});
+
+		webClient.get().uri("/users")
+			.exchange()
+			.expectStatus().is2xxSuccessful()
+			.expectBodyList(User.class)
+			.value(getResponses -> {
+				User getResponse = getResponses.get(0);
+				assertNotNull(getResponse.id());
+				assertEquals(user.username(), getResponse.username());
+				assertEquals(user.email(), getResponse.email());
+				assertEquals(user.password(), getResponse.password());
+			});
 	}
 
 }
